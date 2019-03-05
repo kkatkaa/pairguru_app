4eec2d2c28c91a78916bd197eb5db017da23ed8e
kkatkaa <katka36@tlen.pl>
2019-03-05 20:22:51 +0100

    Add validation: one user can leave only one comment to the movie

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
+  validates :user_id, uniqueness: { scope: :movie_id, message: "you've already made a comment! Click 'edit' to change your comment." }
+
   belongs_to :movie
   belongs_to :user
 end
