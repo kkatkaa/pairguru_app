315f4cbf5766c3bfa5f6f1eaac4a6327b255d45a
kkatkaa <katka36@tlen.pl>
2019-03-07 23:47:43 +0100

    Add query to display top 10 commenders of the week

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @movies = Movie.all
-    @top_commenters = User.joins(:comments).group("users.name").limit(10).order("count(comments.id) desc")
+    @top_commenters = User.joins(:comments).group("users.name").limit(10).order("count(comments.id) desc").by_week(Time.now)
   end
 end
