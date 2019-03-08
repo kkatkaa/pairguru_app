a626cc406f8b97cc6d64c4947bee197ac70cbf0d
kkatkaa <katka36@tlen.pl>
2019-03-08 10:14:34 +0100

    Modify query about displaying top 10 commenters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+  before_action :top_commenters
+
   def index
     @users = User.all
     @movies = Movie.all
-    @top_commenters = User.joins(:comments).group("users.name").limit(10).order("count(comments.id) desc").by_week(Time.now)
+  end
+
+  def top_commenters
+   @top_commenters = User.joins(:comments).group("users.name").limit(10).order("count(comments.id) desc").where(comments: { created_at: 7.days.ago..Time.now })
   end
 end
